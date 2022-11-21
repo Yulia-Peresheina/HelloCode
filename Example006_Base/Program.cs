1fe134a4007b87fb8289e434a28ae4b1383653d8
Yulia <peresheina.yulia@yandex.ru>
2022-11-21 23:31:32 +0300

    Find max, Example006

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 3;
+int b = 4;
+int c = 7;
+int d = 1;
+int e = 9;
+int max = a;
+ if (a > max) max = a;
+ if (b > max) max = b;
+ if (c > max) max = c;
+ if (d > max) max = d;
+ if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
