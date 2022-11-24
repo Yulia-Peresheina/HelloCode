49e7653a4484e615bee2da284824710b6fbd9efc
Yulia <peresheina.yulia@yandex.ru>
2022-11-24 23:36:45 +0300

    Example010 ArrayMethod add

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти в массиве индекс числа
+
+int[] array = {1, 3, 56, 2, 87, 69, 56, 90, 10};
+
+int N = 56;
+//int n = array.Length;
+int index = 0;
+
+ while (index < array.Length)
+ {
+    if (array[index] == N)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
+
